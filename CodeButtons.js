539df3993013c4d62014d383bb14d30a328e156f
SaikoZen <44778722+SaikoZen@users.noreply.github.com>
2018-11-09 13:52:13 +0200

    Add files via upload
    add code

---
+++ CodeButtons.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    var makeRed = function(){
+    var button = $(this);
+
+        $("#button1").click(makeRed);
+        $("#button2").click(makeRed);
+        $("#button3").click(makeRed);
+        $("#button4").click(makeRed);
+        $("#button5").click(makeRed);
+    
+    }
+});    
