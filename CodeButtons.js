772b9206f3ca84d2df97ef7076143991a6cb08dc
SaikoZen <44778722+SaikoZen@users.noreply.github.com>
2018-11-21 11:49:42 +0200

    Add files via upload
    cosmetics

--- CodeButtons.js
+++ CodeButtons.js
@@ -1,17 +1,72 @@
 $(document).ready(function(){
     
-    var makeRed = function($(this).addClass("redButton") {
-    $("#button1").click(makeRed);
-    });  
+    var button = $("button").addClass("defaultButton");
+    
+    $("#reset").click(button);
 
-    $("#button2").click(function(){   
-        $(this).addClass("redButton");    
+    var makeRed = $("button").click(function(){
+
+    $(this).addClass("redButton");
+    
+    })
+
+/*    $("#button1").click(function(){
+        $(this).addClass("redButton");
+    })
+    $("#button2").click(function(){
+        $(this).addClass("redButton");
     })
     $("#button3").click(function(){
-        $(this).addClass("redButton"); 
+        $(this).addClass("redButton");
     })
     $("#button5").click(function(){
         $(this).addClass("redButton"); 
-    })
+    }) */
+
+    $("#button4").click(function(){
+        $(this).addClass("greenButton");
+        
+        $("#button2").click(function(){
+            $(this).addClass("greenButton");
+
+            $("#button3").click(function(){
+                $(this).addClass("greenButton");
+
+                $("#button5").click(function(){
+                    $(this).addClass("greenButton");
+
+                    $("#button1").click(function(){
+                        $(this).addClass("greenButton");
+                        alert("You have unlocked the code");
+                        $("#reset").show();
+
+                          $("#reset").click(function(){
+                            $("button").addClass(button);   
+                          })
+                    })
+                })
+            })
+        })
+
+    }); 
+
   
+    
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
